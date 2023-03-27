1a0ef2c1911500d875d6f41e7906d1995ddf6bd5
“eladyi” <elad.yaish@gmail.com>
2023-03-27 13:39:31 +0300

    add navigation

--- app/navigation/RootNavigation.js
+++ app/navigation/RootNavigation.js
@@ -3,15 +3,31 @@ import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from '../screen/HomeScreen/HomeScreen';
+import Login from '../screen/Login/LoginScreen';
+import OtpScreen from '../screen/Login/OtpScreen';
 
-function RootNavigatin() {
 
-    const Stack = createNativeStackNavigator();
+const RootNavigatin = () => {
   
+    const Stack = createNativeStackNavigator();
+    
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={Login} options={{ 
+            title: 'הפניקס',
+            headerStyle: {
+              backgroundColor: 'gray',
+            },
+           }
+            } />
+            <Stack.Screen name="Otp" component={OtpScreen} options={{ 
+            title: 'הפניקס',
+            headerStyle: {
+              backgroundColor: 'green',
+            },
+           }
+            } />
         </Stack.Navigator>
       </NavigationContainer>
     );
